345fe462f1b541f19e1d7996de4dc7ee96cff908
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-04-12 00:22:57 +0700

    Array Map Javascript

--- belajar-javascript-oop/9.Reusable_Function/arrayMap.js
+++ belajar-javascript-oop/9.Reusable_Function/arrayMap.js
@@ -1,3 +1,9 @@
+/*
+
+  Fungsi map() pada JavaScript adalah salah satu method bawaan dari objek Array yang digunakan untuk membuat array baru dengan memodifikasi setiap elemen pada array yang diberikan.
+
+*/
+
 const newArray = ['Thomas', 'Ridho', 'Frans', 'Irma'].map((name) => {
   return `${name}`;
 });
