a3cc76bf4cb9b27802e7491501ce287094bfd429
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-04-12 00:16:38 +0700

    Array Filter Javascript

---
+++ belajar-javascript-oop/9.Reusable_Function/arrayFilter.js
@@ -0,0 +1,27 @@
+const truthyArray = [1, '', 'Hallo', 0, null, 'Harry', 14].filter((item) =>
+  Boolean(item)
+);
+console.log(truthyArray);
+
+// Contoh 2
+const students = [
+  {
+    name: 'Harry',
+    score: 60,
+  },
+  {
+    name: 'James',
+    score: 88,
+  },
+  {
+    name: 'Ron',
+    score: 90,
+  },
+  {
+    name: 'Bethy',
+    score: 75,
+  },
+];
+
+const smartStudent = students.filter((student) => student.score > 80);
+console.log(smartStudent);
